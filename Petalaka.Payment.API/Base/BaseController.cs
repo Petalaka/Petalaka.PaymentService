2454a5a1a04ab41e9fe725dfedfade8c01dd2c0f
kanatsumi <ducnase171103@fpt.edu.vn>
2024-10-23 17:32:00 +0700

    fix base route

--- Petalaka.Payment.API/Base/BaseController.cs
+++ Petalaka.Payment.API/Base/BaseController.cs
@@ -4,7 +4,7 @@ using System.Text.RegularExpressions;
 namespace Petalaka.Payment.API.Base
 {
     [ApiController]
-    [Route("api/pet-store-service/[controller]")]
+    [Route("api/payment-service/[controller]")]
     [ServiceFilter(typeof(ValidateModelStateAttribute))]
     public abstract class BaseController : ControllerBase
     {
